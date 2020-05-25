80a73d64487bd999d9271d00859e78c9f30c670c
jaybenaim <benaimjacob@gmail.com>
2020-05-25 19:00:56 -0400

    data limiter created for /repos

--- api/controllers/repo.js
+++ api/controllers/repo.js
@@ -1,7 +1,15 @@
 const repos = require("../repos");
 
 const getRepos = (req, res) => {
-  return res.status(200).send(...repos);
+  let currentRepos = repos[0];
+  let { start_at: startAt } = req.query;
+  if (!startAt) {
+    startAt = 0;
+  }
+  let endAt = Number(startAt) + 20;
+  let next20Repos = currentRepos.slice(startAt, endAt);
+
+  return res.status(200).send(next20Repos);
 };
 
 module.exports = { getRepos };
