1d71e39964e5ca7d7769effa91b611449e727d78
jaybenaim <benaimjacob@gmail.com>
2020-05-31 16:56:40 -0400

    created find route

--- api/controllers/repo.js
+++ api/controllers/repo.js
@@ -14,9 +14,38 @@ const getRepos = (req, res) => {
 const filterRepos = (req, res) => {
   let currentRepos = repos[0];
   let { filter } = req.query;
-  let results = currentRepos.filter((repo) =>
-    repo.name.toLowerCase().includes(filter.toLowerCase())
-  );
-  res.status(200).send(results);
+
+  if (filter === "favourites") {
+    let repos = currentRepos.filter((repo) => {
+      let name = repo.name.toLowerCase();
+
+      return (
+        name.includes("dolce") ||
+        name.includes("isell") ||
+        name.includes("estate") ||
+        name.includes("job") ||
+        name.includes("dev") ||
+        name === "card_games" ||
+        name.includes("shop_it_django") ||
+        name.includes("html5")
+      );
+    });
+    res.status(200).send(repos);
+  }
+  if (filter === "wdi") {
+    let repos = currentRepos.filter((repo) => {
+      let name = repo.name.toLowerCase();
+
+      return (
+        name.includes("bit") || name.includes("wdi") || name.includes("day")
+      );
+    });
+    res.status(200).send(repos);
+  } else {
+    let repos = currentRepos.filter((repo) =>
+      repo.name.toLowerCase().includes(filter.toLowerCase())
+    );
+    res.status(200).send(repos);
+  }
 };
 module.exports = { getRepos, filterRepos };
