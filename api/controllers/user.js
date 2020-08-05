a15a03a9772a4fa023a0f13ec26d36d31419bc77
jaybenaim <benaimjacob@gmail.com>
2020-08-05 15:36:42 -0400

    changed fetch userInfo to live fetch instead of saved data

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,7 +1,16 @@
 const userInfo = require("../userInfo");
 
 const getUser = (req, res) => {
-  return res.status(200).send(userInfo);
+    return await axios.get(
+    `https://api.github.com/repos/jaybenaim/`,
+    {
+      auth: {
+        username: process.env.GH_USERNAME,
+        token: process.env.GH_TOKEN,
+      },
+    }
+  );
+  // return res.status(200).send(userInfo);
 };
 
 module.exports = { getUser };
